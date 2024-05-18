6a565bea666b41cd7e001471ceffa2708972ce6c
alvarez15888 <alvarez15888@gmail.com>
2024-05-18 16:29:34 +0200

    20240518

---
+++ ejercicio4/index.js
@@ -0,0 +1,18 @@
+// inicializar 
+const express = require('express')
+const userRoute = require('./routes/users')
+
+const app = express()
+
+const PORT = 8080
+
+
+require('./database')
+app.use(express.json())
+
+app.use('/api/users',userRoute)
+
+app.listen(
+    PORT,
+    () => console.log(`Estoy en http://localhost:${PORT}`)
+)
