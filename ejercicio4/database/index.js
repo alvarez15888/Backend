a55d3bedd05eeff3aa5fc905c16d0425d887e668
alvarez15888 <alvarez15888@gmail.com>
2024-05-18 22:53:24 +0200

    Tarea2

--- ejercicio4/database/index.js
+++ ejercicio4/database/index.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydb')
-.then(() => console.log('La conexion ha sido exitosa'))
-.catch((err) => console.log(err))
+// mongoose.connect('mongodb://127.0.0.1:27017/mydb')
+// .then(() => console.log('La conexion ha sido exitosa'))
+// .catch((err) => console.log(err))
