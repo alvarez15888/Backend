fd595e8d409fd142b70c395e7571d6b615ccd0d3
alvarez15888 <alvarez15888@gmail.com>
2024-06-20 14:49:18 +0200

    tarea4

--- project_TFM_2/backend/lost_found/tests.py
+++ project_TFM_2/backend/lost_found/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import DjangoUser
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class FoundCreationTestCase(APITestCase):
+    def setUp(self):
+        self.found = DjangoUser.objects.create_superuser(username='test', password='test')
+        self.token = Token.objects.create(user = self.found)
+        self.client.credentials(HTTP_AUTHORIZATION = 'Token' + self.token.key)
+
+    def test_found_creation(self):
+        dataf = {'obj':'Samnsung', 'serial':'1234WER456'}
+        resf = self.client.post('/api_view/found/', dataf)
+        self.assertEqual(resf.status_code, status.HTTP_201_CREATED)
+
+    def test_found_creation_no_authorized(self):
+        self.client.force_authenticate(found=None)
+        dataf = {'obj':'Samnsung', 'serial':'1234WER456'}
+        resf = self.client.post('/api_view/found/', dataf)
+        self.assertEqual(resf.status_code, status.HTTP_403_FORBIDDEN)
--- project_TFM_2/backend/tfm/settings.py
+++ project_TFM_2/backend/tfm/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "tfm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "found",
+        "USER": 'root',
+        "PASSWORD": 'Engenharia',
+        "HOST": 'localhost',
+        "PORT": '3306'
     }
 }
 
